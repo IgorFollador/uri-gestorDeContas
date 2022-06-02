edb350152a9cf1f5e83a8a3b067409933b30d8b4
Igor Follador <igorledf@hotmail.com>
2022-06-01 21:48:52 -0300

    fix: Merge FormaDePagamento

--- routes/web.php
+++ routes/web.php
@@ -28,10 +28,3 @@ Route::group(['prefix'=>'usuarios', 'where'=>['id'=>'[0-9]+']], function() {
 Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
